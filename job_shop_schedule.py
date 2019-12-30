0478f7f969c0aef16ecc49353e022c69b68e0b40
Simon Iribarren <simon.godoy@check24.de>
2019-12-30 19:24:27 +0100

    add brute_force and improve local_search

--- job_shop_schedule.py
+++ job_shop_schedule.py
@@ -65,38 +65,71 @@ def calculate_total_time(times, machines, config, n):
             current_machine = job_machines[j] - 1
             usage_time = job_times[j]
             machine_usage = USED[current_machine]
+            current_time, total_time = fillTimeSlot(machine_usage, current_time, usage_time, job, total_time)
 
-            if len(machine_usage) > 0:
-                prev = 0
-                slot = None
-                for start, end in machine_usage:
-                    if start < current_time and current_time < end:
-                        current_time = end
-
-                    if prev == 0 and start > current_time + usage_time:
-                        slot = [current_time, usage_time + current_time]
-                        break
-                    elif start - prev >= usage_time and current_time <= prev:
-                        slot = [current_time, current_time + usage_time]
-                        break
-
-                    prev = end
-                    if end > current_time:
-                        current_time = end
-
-                if slot == None:
-                    slot = [current_time, current_time + usage_time]
-
-                current_time = slot[1]
-                machine_usage.append(slot)
-                machine_usage.sort(key=lambda x: x[1])
-
-                if slot[1] > total_time:
-                    total_time = slot[1]
-            else: 
-                machine_usage.append([current_time, usage_time + current_time])
-                current_time += usage_time;
     return total_time, USED
+
+def calculate_time(times, machines, config, n):
+    time_table = []
+    for i in range(len(machines[0])):
+        time_table.append([])
+
+    current_times = [0]*n
+    job_progress = [0]*n
+    total_time = 0
+
+    for j in config:
+        job = j%n
+        current_machine = machines[job][job_progress[job]] - 1
+        current_time = current_times[job]
+        machine_usage = time_table[current_machine]
+        usage_time = times[job][job_progress[job]]
+        current_time, total_time = fillTimeSlot(machine_usage, current_time, usage_time, job, total_time)
+        job_progress[job] += 1
+        current_times[job] = current_time
+
+    return total_time, time_table
+
+
+def fillTimeSlot(machine_usage, current_time, usage_time, job, total_time):
+    if len(machine_usage) > 0:
+        prev = 0
+        slot = None
+        for used_slots in machine_usage:
+            start = used_slots[0]
+            end = used_slots[1]
+
+            if start < current_time and current_time < end:
+                current_time = end
+
+            if prev == 0 and start > current_time + usage_time:
+                slot = [current_time, usage_time + current_time, job]
+                break
+            elif start - prev >= usage_time and current_time <= prev:
+                slot = [current_time, current_time + usage_time, job]
+                break
+
+            prev = end
+            if end > current_time:
+                current_time = end
+
+        if slot == None:
+            slot = [current_time, current_time + usage_time, job]
+
+        current_time = slot[1]
+        machine_usage.append(slot)
+        machine_usage.sort(key=lambda x: x[1])
+
+        if slot[1] > total_time:
+            total_time = slot[1]
+
+    else: 
+        machine_usage.append([current_time, usage_time + current_time, job])
+        current_time += usage_time;
+
+    return current_time, total_time
+        
+
 
 def get_neightbor(config):
     list_cp = copy.copy(config)
@@ -107,15 +140,41 @@ def get_neightbor(config):
     list_cp[id2] = tmp
     return list_cp
 
+from itertools import permutations 
+
+def brute_force(times, machines, n):
+    configuration = [10, 13, 12, 11, 11, 9, 8, 7, 7, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 12, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 14, 9, 8, 7, 6, 5, 4, 3, 11, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 10, 4, 3, 2, 9, 0, 14, 13, 12, 11, 10, 9, 8, 6, 5, 5, 4, 3, 12, 5, 0, 3, 13, 12, 11, 10, 9, 8, 7, 1, 5, 4, 3, 0, 8, 0, 14, 13, 12, 11, 10, 9, 8, 7, 5, 6, 4, 3, 2, 1, 2, 6, 13, 1, 5, 10, 9, 1, 3, 6, 5, 4, 3, 2, 12, 0, 14, 13, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 1, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 14, 7, 1, 4, 14, 2, 6, 0, 14, 14, 2, 11, 10, 9, 8, 7, 6, 4, 4, 7, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 6, 3, 2, 1, 0, 14, 13, 12, 2, 10, 9, 8, 7, 6, 4, 5, 3, 2, 1, 0]
+    best_result, best_table = calculate_total_time(times, machines, configuration, n)
+
+    for cfg in permutations(configuration, len(configuration)):
+        result, table = calculate_time(times, machines, cfg, n)
+        if result < best_result: 
+            best_result = result
+            best_table = table
+            best_config = copy.copy(configuration)
+            print(best_result)
+    
+    print("TABLE:")
+    i = 1
+    for row in best_table:
+        print("M%s: %s" %(i, row))
+        i += 1
+
+    print("RESULT: %s" %best_result)
+    print("ORDER: %s" %best_config)     
+      
+
 
 def local_search(times, machines, n):
-    configuration = range(n)
+    configuration = range(n*n)
     best_result, best_table = calculate_total_time(times, machines, configuration, n)
     best_config = copy.copy(configuration)
     
-    for i in range(10000):
+    print("####Init sequencial config ####" )  
+
+    for i in range(100000):
         cfg = get_neightbor(configuration)
-        result, table = calculate_total_time(times, machines, cfg, n)
+        result, table = calculate_time(times, machines, cfg, n)
         if result < best_result: 
             configuration = cfg
             best_result = result
@@ -123,29 +182,57 @@ def local_search(times, machines, n):
             best_config = copy.copy(configuration)
             print(best_result)
 
+    print("SEQUENTIAL BEST: %s" %best_result)        
+
+    configuration = range(n)*n
     configuration = configuration[::-1]
-
-    for i in range(10000):
-        cfg = get_neightbor(configuration)
-        result, table = calculate_total_time(times, machines, cfg, n)
-        if result < best_result: 
+    print("####Init reverse config ####" )
+    reverse_best = 1000000
+    cfg = configuration
+    for i in range(100000):
+        result, table = calculate_time(times, machines, cfg, n)
+        if result < reverse_best: 
             configuration = cfg
-            best_result = result
-            best_table = table
-            best_config = copy.copy(configuration)
-            print(best_result)
+            reverse_best = result
+            if reverse_best < best_result:
+                best_result = reverse_best
+                best_table = table
+                best_config = copy.copy(configuration)
+                print(best_result)
+        cfg = get_neightbor(configuration)
 
+    print("REVERSE BEST: %s" %reverse_best)        
+
+    
+    random.shuffle(configuration)
+    
+    best_random = 1000000
+    cfg = configuration
+
+    print("####Init random config ####" )
     for i in range(10000):
-        random.shuffle(configuration)
-        result, table = calculate_total_time(times, machines, configuration, n)
-        if result < best_result: 
-            best_result = result
-            best_table = table
-            best_config = copy.copy(configuration)
-            print(best_result)
+        result, table = calculate_time(times, machines, cfg, n)
+        if(result < best_random):
+            configuration = cfg
+            best_random = result
+            if best_random < best_result:
+                best_result = best_random
+                best_table = table
+                best_config = copy.copy(configuration)
+                print(best_result)
+        cfg = get_neightbor(configuration)
+
+    print("RANDOM BEST: %s" %best_random)        
+
+    print("OVERALL RESULT")
+    print("TABLE:")
+    i = 1
+    for row in best_table:
+        print("M%s: %s" %(i, row))
+        i += 1
 
     print("RESULT: %s" %best_result)        
     print("ORDER: %s" %best_config)     
    
-times, machines, n = readFileDiff("test")
-local_search(times, machines, n)
+times, machines, n = readFile("test")
+brute_force(times, machines, n)
