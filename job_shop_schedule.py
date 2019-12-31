94e5097e55aa70ae841dc49eddb45bcb540e8328
Simon Iribarren <simon.godoy@check24.de>
2019-12-31 10:21:33 +0100

    initial ga implemented

--- job_shop_schedule.py
+++ job_shop_schedule.py
@@ -1,30 +1,6 @@
 import random
 import copy
 
-
-def readFileDiff(filepath):
-    times_done = False
-    times = []
-    machines = []
-
-    with open(filepath) as fp:
-        line = fp.readline()
-        while line:
-            raw_line = line.strip().split('  ')
-            curr_time = []
-            curr_machine = []
-            time = False
-            for pair in raw_line:
-                machine, time = pair.split(' ')
-                curr_time.append(int(time))
-                curr_machine.append(int(machine))
-    
-            times.append(curr_time)
-            machines.append(curr_machine)
-
-            line = fp.readline()
-    return times, machines, len(times)
-
 def readFile(filepath):
     times_done = False
     times = []
@@ -50,28 +26,11 @@ def readFile(filepath):
             line = fp.readline()
     return times, machines, len(times)
 
-def calculate_total_time(times, machines, config, n):
-    USED = []
-    for i in range(len(machines[0])):
-        USED.append([])
-    
-    total_time = 0
-    for i in range(n):
-        job = config[i]
-        job_times = times[job]
-        job_machines = machines[job]
-        current_time = 0
-        for j in range(len(job_machines)):
-            current_machine = job_machines[j] - 1
-            usage_time = job_times[j]
-            machine_usage = USED[current_machine]
-            current_time, total_time = fillTimeSlot(machine_usage, current_time, usage_time, job, total_time)
-
-    return total_time, USED
 
 def calculate_time(times, machines, config, n):
     time_table = []
-    for i in range(len(machines[0])):
+    machine_number = len(machines[0])
+    for i in range(machine_number):
         time_table.append([])
 
     current_times = [0]*n
@@ -80,11 +39,13 @@ def calculate_time(times, machines, config, n):
 
     for j in config:
         job = j%n
-        current_machine = machines[job][job_progress[job]] - 1
+        current_machine = machines[job][job_progress[job]] - 1 
         current_time = current_times[job]
         machine_usage = time_table[current_machine]
         usage_time = times[job][job_progress[job]]
+
         current_time, total_time = fillTimeSlot(machine_usage, current_time, usage_time, job, total_time)
+
         job_progress[job] += 1
         current_times[job] = current_time
 
@@ -140,89 +101,95 @@ def get_neightbor(config):
     list_cp[id2] = tmp
     return list_cp
 
-from itertools import permutations 
+def swap_rnd(config):
+    id1 = random.choice(range(len(config)))
+    id2 = random.choice(range(len(config)))
+    tmp = config[id1]
+    config[id1] = config[id2]
+    config[id2] = tmp
+    return config
 
-def brute_force(times, machines, n):
-    configuration = [10, 13, 12, 11, 11, 9, 8, 7, 7, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 13, 12, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 14, 9, 8, 7, 6, 5, 4, 3, 11, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 10, 4, 3, 2, 9, 0, 14, 13, 12, 11, 10, 9, 8, 6, 5, 5, 4, 3, 12, 5, 0, 3, 13, 12, 11, 10, 9, 8, 7, 1, 5, 4, 3, 0, 8, 0, 14, 13, 12, 11, 10, 9, 8, 7, 5, 6, 4, 3, 2, 1, 2, 6, 13, 1, 5, 10, 9, 1, 3, 6, 5, 4, 3, 2, 12, 0, 14, 13, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 1, 8, 7, 6, 5, 4, 3, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 14, 7, 1, 4, 14, 2, 6, 0, 14, 14, 2, 11, 10, 9, 8, 7, 6, 4, 4, 7, 2, 1, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 6, 3, 2, 1, 0, 14, 13, 12, 2, 10, 9, 8, 7, 6, 4, 5, 3, 2, 1, 0]
-    best_result, best_table = calculate_total_time(times, machines, configuration, n)
 
-    for cfg in permutations(configuration, len(configuration)):
-        result, table = calculate_time(times, machines, cfg, n)
-        if result < best_result: 
-            best_result = result
-            best_table = table
-            best_config = copy.copy(configuration)
-            print(best_result)
+def removeFromList(parent, list):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in parent if not (x in list or seen_add(x))]
+
+def genetic(times, machines, n, population_number, generations, rate):
+    machine_number = len(machines[0])
+    base_arr = range(n*machine_number)
+
+    def generate_population(number, n):
+        population = []
+        for i in range(number):
+            ind = range(n*machine_number)
+            random.shuffle(ind)
+            population.append(ind)
+
+        population.append(range(n*machine_number))
+        return population
+
+
+    def crossover(father, mother, start_index, end_index):
+        sperm = father[start_index:end_index]
+        fetus = removeFromList(mother, sperm)
+        result = [];
+        result.extend(fetus[:start_index])
+        result.extend(sperm)
+        result.extend(fetus[start_index:])
+        return result
     
-    print("TABLE:")
-    i = 1
-    for row in best_table:
-        print("M%s: %s" %(i, row))
-        i += 1
+    def mutation(population, mutation_rate):
+        new_population = []
+        for individual in population:
+            if(random.random() > mutation_rate):
+                new_population.append(swap_rnd(individual))
+            else:
+                new_population.append(individual)
+        return new_population
 
-    print("RESULT: %s" %best_result)
-    print("ORDER: %s" %best_config)     
-      
+    def bestIndividual(population, gen):
+        best_individual = None
+        best_result = None
+        for individual in population:
+            result, table = calculate_time(times, machines, individual, n)
+            if not best_result or result < best_result:
+                best_result = result
+                best_individual = individual
+
+        return best_individual, best_result
+
+    def evolve(population, mutation_rate, generation):
+        new_population = []
+        best_ind, best_result = bestIndividual(population, generation)
+
+        for i, ind in enumerate(population):
+            if(i == len(population) - 1):
+                break
+            start_index = random.choice(base_arr)
+            end_index = random.choice(base_arr[start_index:])
+            baby = crossover(ind, population[i+1], start_index, end_index)
+            new_population.append(baby)
+
+        new_population = mutation(new_population, mutation_rate)
+        new_population.append(best_ind)
+        return new_population
 
 
-def local_search(times, machines, n):
-    configuration = range(n*n)
-    best_result, best_table = calculate_total_time(times, machines, configuration, n)
-    best_config = copy.copy(configuration)
+
+    population = generate_population(population_number, n)
+    result = None
+    result_ind = None
+
+    for i in range(generations):
+        population = evolve(population, rate, i)
+        best_ind, best_result = bestIndividual(population, i)
+        if(not result or best_result < result):
+            print("generation %s result %s" %(i, best_result))
+            result = best_result
+            result_ind = best_ind
     
-    print("####Init sequencial config ####" )  
-
-    for i in range(100000):
-        cfg = get_neightbor(configuration)
-        result, table = calculate_time(times, machines, cfg, n)
-        if result < best_result: 
-            configuration = cfg
-            best_result = result
-            best_table = table
-            best_config = copy.copy(configuration)
-            print(best_result)
-
-    print("SEQUENTIAL BEST: %s" %best_result)        
-
-    configuration = range(n)*n
-    configuration = configuration[::-1]
-    print("####Init reverse config ####" )
-    reverse_best = 1000000
-    cfg = configuration
-    for i in range(100000):
-        result, table = calculate_time(times, machines, cfg, n)
-        if result < reverse_best: 
-            configuration = cfg
-            reverse_best = result
-            if reverse_best < best_result:
-                best_result = reverse_best
-                best_table = table
-                best_config = copy.copy(configuration)
-                print(best_result)
-        cfg = get_neightbor(configuration)
-
-    print("REVERSE BEST: %s" %reverse_best)        
-
-    
-    random.shuffle(configuration)
-    
-    best_random = 1000000
-    cfg = configuration
-
-    print("####Init random config ####" )
-    for i in range(10000):
-        result, table = calculate_time(times, machines, cfg, n)
-        if(result < best_random):
-            configuration = cfg
-            best_random = result
-            if best_random < best_result:
-                best_result = best_random
-                best_table = table
-                best_config = copy.copy(configuration)
-                print(best_result)
-        cfg = get_neightbor(configuration)
-
-    print("RANDOM BEST: %s" %best_random)        
+    best_result, best_table = calculate_time(times, machines, best_ind, n)
 
     print("OVERALL RESULT")
     print("TABLE:")
@@ -230,9 +197,13 @@ def local_search(times, machines, n):
     for row in best_table:
         print("M%s: %s" %(i, row))
         i += 1
-
     print("RESULT: %s" %best_result)        
-    print("ORDER: %s" %best_config)     
-   
-times, machines, n = readFile("test")
-brute_force(times, machines, n)
+    print("ORDER: %s" %result_ind) 
+        
+
+
+times, machines, n = readFile("15_15_0")
+
+genetic(times, machines, n, 50, 5000, 0.2)
+
+
